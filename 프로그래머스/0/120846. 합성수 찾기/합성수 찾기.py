6b00d790b534eda0c11a0989a695cbc0989585eb
greenlemonT <143719724+greenlemonT@users.noreply.github.com>
2024-03-22 18:45:25 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -2,6 +2,7 @@ def solution(n):
     arr = []
     answer = 0
     for i in range(2, n+1):
+        arr.clear()
         for j in range(1, i+1):
             if i % j == 0:
                 arr.append(i)
