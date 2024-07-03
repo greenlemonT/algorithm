5107e1b8cad27278962bed30ec54bcd119c58e2f
greenlemonT <143719724+greenlemonT@users.noreply.github.com>
2024-07-03 15:02:56 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,16 @@
+def solution(k, score):
+    answer = []
+    
+    hall = []
+    for i in score:
+        if (len(hall) < k):
+            hall.append(i)
+        else:
+            if (i > min(hall)):
+                hall.remove(min(hall))
+                hall.append(i)
+                
+        hall.sort()
+        answer.append(hall[0])
+        
+    return answer
