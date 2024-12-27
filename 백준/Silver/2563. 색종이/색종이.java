ad801cb190741a103387c691cab0fc9eaceaeeb3
greenlemonT <143719724+greenlemonT@users.noreply.github.com>
2024-12-27 10:36:14 +0900

    [Silver V] Title: 색종이, Time: 180 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean[][] paper = new boolean[100][100];
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt(); 
+            int y = scanner.nextInt();
+
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[j][k] = true; 
+                }
+            }
+        }
+
+        int area = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j]) {
+                    area++;
+                }
+            }
+        }
+
+        System.out.println(area);
+
+        scanner.close();
+    }
+}
