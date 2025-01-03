3d40a5eb0f62005a55958b833e6c45fa2ac8ac3e
greenlemonT <143719724+greenlemonT@users.noreply.github.com>
2025-01-03 15:35:09 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 98.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int current, int[][] computers, boolean[] visited) {
+        visited[current] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[current][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited);
+            }
+        }
+    }
+}
