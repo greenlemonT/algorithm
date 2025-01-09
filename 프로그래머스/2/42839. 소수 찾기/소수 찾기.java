191a6c9b66733c1ae3b15881041dc31a5eeb4793
greenlemonT <143719724+greenlemonT@users.noreply.github.com>
2025-01-09 10:44:59 +0900

    [level 2] Title: 소수 찾기, Time: 30.07 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,37 @@
-import java.util.*;
+import java.util.HashSet;
 
-    class Solution {
-        public int solution(String numbers) {
-            Set<Integer> primes = new HashSet<>();
-            char[] digits = numbers.toCharArray();
-            boolean[] visited = new boolean[digits.length];
 
-            // 모든 가능한 숫자 조합 생성
-            generateNumbers(digits, visited, "", primes);
+class Solution {
+    public int solution(String numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        permutation("", numbers, set);
+        int count = 0;
 
-            // 소수의 개수 반환
-            return primes.size();
-        }
-
-        private void generateNumbers(char[] digits, boolean[] visited, String current, Set<Integer> primes) {
-            // 현재 조합이 숫자인 경우, 소수 여부 확인
-            if (!current.isEmpty()) {
-                int num = Integer.parseInt(current);
-                if (isPrime(num)) {
-                    primes.add(num);
-                }
-            }
-
-            // 모든 숫자에 대해 조합 생성
-            for (int i = 0; i < digits.length; i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    generateNumbers(digits, visited, current + digits[i], primes);
-                    visited[i] = false;
-                }
+        for (int num : set) {
+            if (isPrime(num)) {
+                count++;
             }
         }
 
-        private boolean isPrime(int num) {
-            if (num < 2) return false;
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) return false;
-            }
-            return true;
-        }
+        return count;
     }
 
+    private boolean isPrime(int num) {
+        if (num < 2) return false; 
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
 
+    public void permutation(String prefix, String str, HashSet<Integer> set) {
+        int n = str.length();
+        if (!prefix.equals("")) {
+            set.add(Integer.valueOf(prefix)); 
+        }
 
+        for (int i = 0; i < n; i++) {
+            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), set);
+        }
+    }
+}
