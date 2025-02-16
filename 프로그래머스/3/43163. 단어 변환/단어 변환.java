1f5676b36684e4197091717d3c790d6761e3d6ca
greenlemonT <143719724+greenlemonT@users.noreply.github.com>
2025-02-16 21:34:06 +0900

    [level 3] Title: 단어 변환, Time: 1.15 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,51 @@
 import java.util.*;
 
+//bfs : while(!q.isEmpty()){}, 한글자만 다른지 판별함수
+//단어와 count같이 저장 : class Node
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-
-        Queue<String> queue = new LinkedList<>(); 
-        Set<String> set = new HashSet<>(Arrays.asList(words)); 
-
-        if(!set.contains(target)){
-            return 0;
-        }
-
-        queue.offer(begin);
-        set.remove(begin);
-
-        while (!queue.isEmpty()){
-            for (int i=0; i<queue.size(); i++){
-                String current = queue.poll();
-
-                if (current.equals(target)){
-                    return answer;
-                }
-                
-                for (String word : set.toArray(new String[set.size()])){
-                    if(canConvert(current,word)){
-                        queue.offer(word);
-                        set.remove(word);
-                    }
+        Queue<Node> q= new LinkedList<>();
+        Set<String> set=new HashSet<>();
+        
+        q.offer(new Node(begin,0));
+        set.add(begin);
+        
+        while(!q.isEmpty()){
+            Node current=q.poll();
+            
+            if(current.word.equals(target)){
+                return current.count;
+            }
+            for(String word:words){
+                if(!set.contains(word)&&check(current.word,word)){
+                    q.offer(new Node(word,current.count+1));
+                    set.add(word);
                 }
             }
-
-            answer++;
+            
         }
-
         return 0;
     }
 
-    private boolean canConvert(String word1, String word2){
-        int diffCnt = 0;
-        for (int i = 0; i<word1.length(); i++){
-            if (word1.charAt(i) != word2.charAt(i)){
-                diffCnt++;
-            }
+    private boolean check(String word1, String word2){
+        int count=0;
+        for(int i=0;i<word1.length();i++){
+            if(word1.charAt(i)!=word2.charAt(i)){
+                count++;
+                if(count>1){
+                    return false;
+                }
+            }        }
+        return count==1;
+    }
+    
+    static class Node{
+        String word;
+        int count;
+        
+        Node(String word,int count){
+            this.word=word;
+            this.count=count;
         }
-        return diffCnt ==1;
     }
 }
